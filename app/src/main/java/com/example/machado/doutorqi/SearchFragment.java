105d1eb2177a884a3cf50bdef7ed4b2993683969
Igor Cesar <igorctorres@hotmail.com>
2015-07-01 00:18:19 -0300

    tela de busca

--- app/src/main/java/com/example/machado/doutorqi/SearchFragment.java
+++ app/src/main/java/com/example/machado/doutorqi/SearchFragment.java
@@ -5,7 +5,10 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by igor on 29/06/15.
@@ -18,6 +21,31 @@ public class SearchFragment extends Fragment {
         Bundle args = new Bundle();
         fragmentSearch.setArguments(args);
         return fragmentSearch;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        try {
+            Spinner spinner = (Spinner) getActivity().findViewById(R.id.especialidades_spinner);
+            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                    getActivity(),R.array.especialidades_array, android.R.layout.simple_spinner_item);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spinner.setAdapter(adapter);
+        } catch (Exception e) {
+            Toast.makeText(getActivity(), e.toString(), 1).show();
+        }
+
+        try {
+            Spinner spinner = (Spinner) getActivity().findViewById(R.id.planos_spinner);
+            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                    getActivity(),R.array.planos_array, android.R.layout.simple_spinner_item);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spinner.setAdapter(adapter);
+        } catch (Exception e) {
+            Toast.makeText(getActivity(), e.toString(), 1).show();
+        }
     }
 
     // Store instance variables based on arguments passed
@@ -33,6 +61,7 @@ public class SearchFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_search, container, false);
         /*TextView tvLabel = (TextView) view.findViewById(R.id.tvLabel);
         tvLabel.setText(page + " -- " + title);*/
+
         return view;
     }
 }
